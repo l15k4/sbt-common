e256ea9cc892bbcf4f620cc55b6032db7b01e598
l15k4 <liska.jakub@gmail.com>
2017-02-02 19:21:20 +0000

    new plugins and deps

--- build.sbt
+++ build.sbt
@@ -9,15 +9,16 @@ lazy val `sbt-common` = (project in file("."))
     publishMavenStyle := true,
     pomIncludeRepository := { _ => false}
   ).settings(
-    addSbtPlugin("com.eed3si9n"         % "sbt-buildinfo"         % "0.6.1"),
-    addSbtPlugin("com.timushev.sbt"     % "sbt-updates"           % "0.2.0"),
-    addSbtPlugin("com.jsuereth"         % "sbt-pgp"               % "1.0.0"),
-    addSbtPlugin("com.eed3si9n"         % "sbt-assembly"          % "0.14.3"),
-    addSbtPlugin("com.dwijnand"         % "sbt-dynver"            % "1.1.1"),
-    addSbtPlugin("com.eed3si9n"         % "sbt-buildinfo"         % "0.6.1"),
-    addSbtPlugin("com.frugalmechanic"   % "fm-sbt-s3-resolver"    % "0.9.0"),
-    addSbtPlugin("se.marcuslonnberg"    % "sbt-docker"            % "1.4.0"),
-    addSbtPlugin("net.virtual-void"     % "sbt-dependency-graph"  % "0.8.2"),
-    addSbtPlugin("pl.project13.scala"   % "sbt-jmh"               % "0.2.18"),
-    addCompilerPlugin("com.lihaoyi"     %% "acyclic"              % "0.1.7")
+    addSbtPlugin("com.eed3si9n"                   % "sbt-buildinfo"         % "0.6.1"),
+    addSbtPlugin("com.timushev.sbt"               % "sbt-updates"           % "0.2.0"),
+    addSbtPlugin("com.jsuereth"                   % "sbt-pgp"               % "1.0.0"),
+    addSbtPlugin("com.eed3si9n"                   % "sbt-assembly"          % "0.14.3"),
+    addSbtPlugin("com.dwijnand"                   % "sbt-dynver"            % "1.1.1"),
+    addSbtPlugin("com.eed3si9n"                   % "sbt-buildinfo"         % "0.6.1"),
+    addSbtPlugin("com.frugalmechanic"             % "fm-sbt-s3-resolver"    % "0.9.0"),
+    addSbtPlugin("se.marcuslonnberg"              % "sbt-docker"            % "1.4.0"),
+    addSbtPlugin("net.virtual-void"               % "sbt-dependency-graph"  % "0.8.2"),
+    addSbtPlugin("pl.project13.scala"             % "sbt-jmh"               % "0.2.18"),
+    addCompilerPlugin("com.softwaremill.clippy"   %% "plugin"               % "0.5.0" classifier "bundle"),
+    addCompilerPlugin("com.lihaoyi"               %% "acyclic"              % "0.1.7")
   )
--- src/main/scala/gwi/sbt/Dependencies.scala
+++ src/main/scala/gwi/sbt/Dependencies.scala
@@ -5,19 +5,21 @@ import sbt._
 trait Dependencies {
 
   lazy val akkaVersion                = "2.4.16"
-  lazy val akkaHttpVersion            = "10.0.1"
+  lazy val akkaHttpVersion            = "10.0.3"
   lazy val jacksonVersion             = "2.8.6"
 
   lazy val awsS3                  = "com.amazonaws"                 %  "aws-java-sdk-s3"              % "1.11.68"
   lazy val loggingImplLog4j       = "org.slf4j"                     %  "slf4j-log4j12"                % "1.7.22"
   lazy val loggingImplLogback     = "ch.qos.logback"                %  "logback-classic"              % "1.1.9"
-  lazy val pprint                 = "com.lihaoyi"                   %% "pprint"                       % "0.4.3"
+  lazy val pprint                 = "com.lihaoyi"                   %% "pprint"                       % "0.4.4"
   lazy val guava                  = "com.google.guava"              %  "guava"                        % "21.0"
-  lazy val sparkSketch            = "org.apache.spark"              %% "spark-sketch"                 % "2.0.1"
+  lazy val sparkSketch            = "org.apache.spark"              %% "spark-sketch"                 % "2.1.0"
   lazy val scalaHttp              = "org.scalaj"                    %% "scalaj-http"                  % "2.3.0"
   lazy val commonsNet             = "commons-net"                   %  "commons-net"                  % "3.5"
   lazy val commonsMath            = "org.apache.commons"            % "commons-math3"                 % "3.6"
   lazy val sprayJson              = "io.spray"                      %% "spray-json"                   % "1.3.3"
+  lazy val redis                  = "com.github.etaty"              %% "rediscala"                    % "1.8.0"
+  lazy val algebird               = "com.twitter"                   %% "algebird-core"                % "0.12.4"
   lazy val jodaTime               = Seq(
                                     "joda-time"                     %  "joda-time"                    % "2.9.7",
                                     "org.joda"                      %  "joda-convert"                 % "1.8"
@@ -48,7 +50,7 @@ trait Dependencies {
                                     "com.typesafe.akka"             %% "akka-cluster-tools"           % akkaVersion,
                                     "com.typesafe.akka"             %% "akka-persistence"             % akkaVersion,
                                     "com.hootsuite"                 %% "akka-persistence-redis"       % "0.6.0",
-                                    "ch.megard"                     %% "akka-http-cors"               % "0.1.10",
+                                    "ch.megard"                     %% "akka-http-cors"               % "0.1.11",
                                     "com.github.TanUkkii007"        %% "akka-cluster-custom-downing"  % "0.0.7",
                                     "com.github.romix.akka"         %% "akka-kryo-serialization"      % "0.5.0",
                                     "com.typesafe.akka"             %% "akka-http-testkit"            % akkaHttpVersion             % "test",
@@ -56,7 +58,7 @@ trait Dependencies {
                                   )
   lazy val testingDeps            = Seq(
                                     "com.lihaoyi"                   %  "ammonite"                     % "0.8.2"                     % "test" cross CrossVersion.full,
-                                    "org.scalatest"                 %% "scalatest"                    % "3.0.0"                     % "test",
+                                    "org.scalatest"                 %% "scalatest"                    % "3.0.1"                     % "test",
                                     "com.storm-enroute"             %% "scalameter"                   % "0.8.2"                     % "test"
                                   )
 }
